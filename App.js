5af012d94266d590e3469982c2ba5c3b2edb7943
Wesley <wezzdc@gmail.com>
2018-12-04 22:07:56 +0800

    Updates to style

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { CheckBox, Avatar, Header } from 'react-native-elements'
+import { blue } from 'ansi-colors';
+// import { createSwitchNavigator } from 'react-navigation'; 
+
+// const AppNavigator = createSwitchNavigator({ 
+//      "goToScreenA": ScreenAComponent, 
+//      "goToScreenB": ScreenBComponent,
+//      "goToScreenC": ScreenCComponent, 
+// });
+
+// export default class App extends React.Component { 
+//      render() { 
+//           return <AppNavigator /> 
+//      } 
+// }
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <Button 
+        title={"Hello"} 
+        onPress={() => { console.log('hello'); }}>
+        </Button>
+        <Avatar
+        xlarge
+        rounded
+        source={{uri: "https://www.nationalgeographic.com/content/dam/animals/thumbs/rights-exempt/mammals/d/domestic-dog_thumb.jpg"}}
+        onPress={() => console.log("Works!")}
+        activeOpacity={0.7}
+        containerStyle={{marginTop: 10, marginBottom: 10}}
+        />
+        <Text style={styles.bigblue}>Testing</Text>
+        <CheckBox
+        center
+        title='Do Task A'
+        />
+      </View>           
     );
   }
 }
@@ -18,4 +49,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+  }
 });
