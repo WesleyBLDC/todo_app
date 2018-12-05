977f2ea06c68bee67da0cdcf0e73fd865c0c2363
Wesley <wezzdc@gmail.com>
2018-12-05 21:27:58 +0800

    Create statful functional component counting

--- components/header.js
+++ components/header.js
@@ -12,10 +12,27 @@ class Header extends React.Component{
 
         //how we create properties
         this.textIwant = props.textIwant;
+        //how we create state
+        this.state={
+            number:0
+        };
+
+        this.addNumberWhenPressed=this.addNumberWhenPressed.bind(this);
+    }
+
+    componentDidUpdate(){    //any state changed, run the function inside.
+        console.log(this.state.number);
+    }
+
+    addNumberWhenPressed(){ //how we manipulate state
+        const previousNumber = this.state.number;   //variable that store the current state
+        this.setState({
+            number:previousNumber+1
+        });
     }
     render(){                           
-        return(
-            <Text>{this.textIwant}</Text>
+        return( 
+            <Text onPress={this.addNumberWhenPressed}>{this.textIwant}</Text>
         );
     }
 }
